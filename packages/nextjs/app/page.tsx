4ffb6d72f69dae9f25d86d0bea00321664fd60a0
Nabeel Abrahams <88242933+NabeelAbrahams@users.noreply.github.com>
2024-04-29 03:29:39 +0200

    fixing modal

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -22,24 +22,50 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex sm:flex-col pt-16 py-8 gap-32 bg-gradient-to-t from-[#083A5B] to-[#27262C]">
+      <div className="flex sm:flex-col gap-32 bg-gradient-to-t from-[#083A5B] to-[#27262C] h-screen">
         {/* Modal */}
-        {isModalOpen ? (
-          <div className="relative flex bg-white/10">
-            <button onClick={closeModal} className="absolute">
-              close
-            </button>
-            <CreateBet />
+        <div
+          className={`z-10 fixed top-0 left-0 w-full h-full flex justify-center backdrop-blur-md items-center bg-black bg-opacity-50 transition-opacity ${
+            isModalOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <div className="bg-black backdrop-blur-md rounded-lg p-8 transform transition-transform duration-500 w-1/2">
+            {isModalOpen && (
+              <div className="relative">
+                <button onClick={closeModal} className="absolute top-0 left-0 m-2 text-white rotate-45">
+                  <button title="Add New" class="group cursor-pointer outline-none hover:rotate-90 duration-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="50px"
+                      height="50px"
+                      viewBox="0 0 24 24"
+                      class="stroke-zinc-400 fill-none group-hover:fill-zinc-800 group-active:stroke-zinc-200 group-active:fill-zinc-600 group-active:duration-0 duration-300"
+                    >
+                      <path
+                        d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                        stroke-width="1.5"
+                      ></path>
+                      <path d="M8 12H16" stroke-width="1.5"></path>
+                      <path d="M12 16V8" stroke-width="1.5"></path>
+                    </svg>
+                  </button>
+                </button>
+                <CreateBet />
+              </div>
+            )}
           </div>
-        ) : null}
-        {/* <div className="rounded-3xl border border-black" style={{ flex: 2, width: "20%" }}>
-          <CreateBet />
-        </div> */}
-        <div className="rounded-3xl border border-black">
-          {/* Button to open modal */}
-          <button onClick={openModal}>Open Modal</button>
         </div>
-        <div className="rounded-3xl border border-white" style={{ flex: 5, width: "70%" }}>
+        <div className="">
+          <div className="my-4 flex flex-col items-center gap-2 justify-center">
+            <div className="text-[56px] font-bold font-mono tracking-wide">Are you ready</div>
+            <div className="text-[56px] font-bold font-mono tracking-wide">to duel?</div>
+            {/* Button to open modal */}
+            <button onClick={openModal}>
+              <button className="uppercase relative py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+                create a bet
+              </button>
+            </button>
+          </div>
           <BetHistory />
         </div>
       </div>
--- packages/nextjs/components/bets/BetHistory.tsx
+++ packages/nextjs/components/bets/BetHistory.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useScaffoldContractWrite, useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
-import { Address } from "~~/components/scaffold-eth";
-import { useAccount } from "wagmi";
+import { Contract } from "@wagmi/core";
 import { formatEther, parseEther } from "viem";
-import { Web3 } from "web3"
-import { Contract } from '@wagmi/core'
+import { useAccount } from "wagmi";
+import { Web3 } from "web3";
+import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldContractWrite, useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const BetHistory = () => {
-
   const { address: connectedAddress } = useAccount();
   const [betId, setBetId] = useState("");
   const [betAmount, setBetAmount] = useState("");
@@ -65,9 +64,18 @@ const BetHistory = () => {
   useEffect(() => {
     if (betCreatedHistory) {
       const updatedBetList = betCreatedHistory.map(singleEventBetCreated => {
-        const isBetDeleted: boolean = betDeletedHistory?.some(singleEventBetDeleted => singleEventBetDeleted.args[0] === singleEventBetCreated.args[0]) || false;
-        const isBetAccepted: boolean = betAcceptedHistory?.some(singleEventBetAccepted => singleEventBetAccepted.args[0] === singleEventBetCreated.args[0]) || false;
-        const isBetFinished: boolean = betFinishedHistory?.some(singleEventBetFinished => singleEventBetFinished.args[0] === singleEventBetCreated.args[0]) || false;
+        const isBetDeleted: boolean =
+          betDeletedHistory?.some(
+            singleEventBetDeleted => singleEventBetDeleted.args[0] === singleEventBetCreated.args[0],
+          ) || false;
+        const isBetAccepted: boolean =
+          betAcceptedHistory?.some(
+            singleEventBetAccepted => singleEventBetAccepted.args[0] === singleEventBetCreated.args[0],
+          ) || false;
+        const isBetFinished: boolean =
+          betFinishedHistory?.some(
+            singleEventBetFinished => singleEventBetFinished.args[0] === singleEventBetCreated.args[0],
+          ) || false;
         return { singleEventBetCreated, isBetDeleted, isBetAccepted, isBetFinished };
       });
 
@@ -78,21 +86,69 @@ const BetHistory = () => {
 
   /*
       Obtaining the price data from chainlink is moved from smart contract to here so that the gas fee is lowered
-  */ 
-  const provider = "https://eth-sepolia.g.alchemy.com/v2/oKxs-03sij-U_N0iOlrSsZFr29-IqbuF"
+  */
+  const provider = "https://eth-sepolia.g.alchemy.com/v2/oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
   const web3Provider = new Web3.providers.HttpProvider(provider);
   const web3 = new Web3(web3Provider);
-  const aggregatorV3InterfaceABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+  const aggregatorV3InterfaceABI = [
+    {
+      inputs: [],
+      name: "decimals",
+      outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "description",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+      name: "getRoundData",
+      outputs: [
+        { internalType: "uint80", name: "roundId", type: "uint80" },
+        { internalType: "int256", name: "answer", type: "int256" },
+        { internalType: "uint256", name: "startedAt", type: "uint256" },
+        { internalType: "uint256", name: "updatedAt", type: "uint256" },
+        { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "latestRoundData",
+      outputs: [
+        { internalType: "uint80", name: "roundId", type: "uint80" },
+        { internalType: "int256", name: "answer", type: "int256" },
+        { internalType: "uint256", name: "startedAt", type: "uint256" },
+        { internalType: "uint256", name: "updatedAt", type: "uint256" },
+        { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "version",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
   const addr = "0x694AA1769357215DE4FAC081bf1f309aDC325306";
   const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
 
-  const handleFinish = async (singleEventBetCreated) => {
+  const handleFinish = async singleEventBetCreated => {
     const targetTimestamp = BigInt(singleEventBetCreated.args[5].toString());
 
     try {
       const latestRoundData = await priceFeed.methods.latestRoundData().call();
 
-      if(targetTimestamp < latestRoundData.updatedAt) {
+      if (targetTimestamp < latestRoundData.updatedAt) {
         const phaseId = Number(BigInt(latestRoundData.roundId) >> 64n);
         const aggregatorRoundId = BigInt(latestRoundData.roundId) & BigInt("0xFFFFFFFFFFFFFFFF");
         const firstRoundId = BigInt(latestRoundData.roundId) - aggregatorRoundId + 1n;
@@ -108,56 +164,56 @@ const BetHistory = () => {
           }
         }
 
-        if(isRoundIdFound) {
+        if (isRoundIdFound) {
           const priceDataAtTarget = await priceFeed.methods.getRoundData(roundIdAtTarget).call();
-          const priceAtTargetAsFloat =  (parseFloat(priceDataAtTarget.answer.toString()) / 10**8);
+          const priceAtTargetAsFloat = parseFloat(priceDataAtTarget.answer.toString()) / 10 ** 8;
           const priceAtTargetInWei = parseEther(priceAtTargetAsFloat.toString());
           finishBet({ args: [BigInt(singleEventBetCreated.args[0]), priceAtTargetInWei] });
-        }
-        else
-          console.error("Target Round ID couldnt be found with the latest round id:", latestRoundData.roundId);
+        } else console.error("Target Round ID couldnt be found with the latest round id:", latestRoundData.roundId);
       } else {
         console.error("Bet is not completed yet!");
       }
-
     } catch (error) {
       console.error("Error:", error);
     }
   };
-  
-  const handleDelete = (singleEventBetCreated) => {
+
+  const handleDelete = singleEventBetCreated => {
     deleteBet({ args: [BigInt(singleEventBetCreated.args[0])] });
   };
 
-  const handleAccept = (singleEventBetCreated) => {
+  const handleAccept = singleEventBetCreated => {
     setBetId(singleEventBetCreated.args[0].toString());
     setBetAmount(singleEventBetCreated.args[2].toString());
-    acceptBet({ args: [BigInt(singleEventBetCreated.args[0])], value: BigInt(singleEventBetCreated.args[2].toString()) });
+    acceptBet({
+      args: [BigInt(singleEventBetCreated.args[0])],
+      value: BigInt(singleEventBetCreated.args[2].toString()),
+    });
   };
 
   const formatTimestamp = (timestamp: number): string => {
     const date = new Date(timestamp * 1000);
     const options: Intl.DateTimeFormatOptions = {
-      year: '2-digit',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
     };
-    return new Intl.DateTimeFormat('en-US', options).format(date).toString();
+    return new Intl.DateTimeFormat("en-US", options).format(date).toString();
   };
 
   return (
-    <div className="px-8 py-12 text-gray-700">
+    <div className="px-8">
       {isLoadingHistory ? (
-          <strong> Loading... </strong>
+        <strong> Loading... </strong>
       ) : (
         <div>
-          <span className="text-center mb-4 block text-2xl font-bold">Bet History</span>
-          <div className="overflow-x-auto rounded-xl" style={{ flex: 1, fontSize: '1.0em' }}>
+          <span className="mb-4 block text-2xl font-bold">Bet History</span>
+          <div className="overflow-x-auto rounded-xl" style={{ flex: 1, fontSize: "1.0em" }}>
             <table className="table w-full">
               <thead>
-                <tr className="bg-primary text-center" style={{ fontSize: '1.2em' }}>
+                <tr className="bg-primary text-center" style={{ fontSize: "1.2em" }}>
                   <th>ID</th>
                   <th>Created by</th>
                   <th>Amount</th>
@@ -179,9 +235,15 @@ const BetHistory = () => {
                     return (
                       <tr key={parseInt(singleEventBetCreated.args[0].toString())}>
                         <td>{parseInt(singleEventBetCreated.args[0].toString())}</td>
-                        <td><Address address={singleEventBetCreated.args[1]} /></td>
+                        <td>
+                          <Address address={singleEventBetCreated.args[1]} />
+                        </td>
                         <td>{parseFloat(formatEther(singleEventBetCreated.args[2])).toFixed(4)}</td>
-                        <td>{singleEventBetCreated.args[3] !== "" ? (parseFloat(singleEventBetCreated.args[3].toString()) / 10**18).toFixed(2) : 0}</td>
+                        <td>
+                          {singleEventBetCreated.args[3] !== ""
+                            ? (parseFloat(singleEventBetCreated.args[3].toString()) / 10 ** 18).toFixed(2)
+                            : 0}
+                        </td>
                         <td>{singleEventBetCreated.args[4] ? "higher" : "lower"}</td>
                         <td>{formatTimestamp(parseInt(singleEventBetCreated.args[5].toString()))}</td>
                         <td className="items-center text-center">
@@ -190,7 +252,10 @@ const BetHistory = () => {
                           ) : isBetAccepted ? (
                             <div className="flex flex-col items-center justify-center gap-2">
                               <span>Accepted</span>
-                              <button className="btn btn-secondary h-[2rem] min-h-[1.5rem]" onClick={() => handleFinish(singleEventBetCreated)}>
+                              <button
+                                className="btn btn-secondary h-[2rem] min-h-[1.5rem]"
+                                onClick={() => handleFinish(singleEventBetCreated)}
+                              >
                                 Finish bet!
                               </button>
                             </div>
@@ -200,11 +265,17 @@ const BetHistory = () => {
                             <div className="flex flex-col items-center justify-center gap-2">
                               <span>Waiting...</span>
                               {singleEventBetCreated.args[1] === connectedAddress ? (
-                                <button className="btn btn-secondary h-[2rem] min-h-[1.5rem]" onClick={() => handleDelete(singleEventBetCreated)}>
+                                <button
+                                  className="btn btn-secondary h-[2rem] min-h-[1.5rem]"
+                                  onClick={() => handleDelete(singleEventBetCreated)}
+                                >
                                   Delete bet!
                                 </button>
                               ) : (
-                                <button className="btn btn-secondary h-[2rem] min-h-[1.5rem]" onClick={() => handleAccept(singleEventBetCreated)}>
+                                <button
+                                  className="btn btn-secondary h-[2rem] min-h-[1.5rem]"
+                                  onClick={() => handleAccept(singleEventBetCreated)}
+                                >
                                   Accept bet!
                                 </button>
                               )}
